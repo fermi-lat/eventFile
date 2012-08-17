6d97251f14810f8545d7003ad5b8eead4a17198f
jrb <>
2012-08-17 18:48:34 +0000

    patches for Windows no-install-headers

--- eventFileLib.py
+++ eventFileLib.py
@@ -2,6 +2,8 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['eventFile'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'eventFile') 
     env.Tool('facilitiesLib')
 
 def exists(env):
